bb988ba8c3ba006013cdc77314c3e2780ab0b543
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2020-11-27 12:45:02 -0600

    custom email added

--- server/email/account.js
+++ server/email/account.js
@@ -3,8 +3,8 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const msg = {
-    to: 'haidranoid16@gmail.com', // Change to your recipient
-    from: 'martinezlara_joseeduardo@hotmail.com', // Change to your verified sender
+    to: 'martinezlara_joseeduardo@hotmail.com', // Change to your recipient
+    from: 'j.martinez@admdelcentro.com', // Change to your verified sender
     subject: 'Sending with SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
     //html: '<strong>k patza mario</strong>',
